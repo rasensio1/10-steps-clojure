defc82918c320ce4cf74f7822acaefcaf3aa4760
Ryan Asensio <ryan.asensio@gmail.com>
2016-01-18 09:00:31 -0700

    initial-commit

---
+++ src/steps_clojure/core.clj
@@ -0,0 +1,6 @@
+(ns steps-clojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/steps_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns steps-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [steps-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
